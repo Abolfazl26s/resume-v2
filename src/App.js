1c1c81b49083d3d17aa2ee4f02e6dd11a3b3246a
Abolfazl <68917563+Abolfazl26s@users.noreply.github.com>
2023-10-04 10:50:29 +0330

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import PortfolioPage from './pages/PortfolioPage';
 import SkillPage from './pages/SkillPage';
 import InfoPage from './pages/InfoPage';
-import HomePage from './pages/HomePage';
 import CertificatePage from './pages/CertificatePage';
 
 import { Provider } from 'react-redux';
@@ -13,7 +12,7 @@ function App() {
   return (
     <Provider store={store}>
     <Routes>
-      <Route path="/" element={<HomePage/>}>
+      <Route path="/" >
         <Route index element={<InfoPage />} />
         <Route path="portfolio" element={<PortfolioPage />} />
         <Route path="skills" element={<SkillPage />} />
